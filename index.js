ec0e5f35bf5a78a84dd3b8ea9b6f50e103374f5b
Randall Burgess <randallallenburgess@gmail.com>
2020-02-02 11:36:13 -0800

    added generate html function.  having issues executing function.  giving unexpected end of input message

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const fs = require('fs');
 const util = require('util');
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 function promptUser() {
 return inquirer.prompt([{
@@ -20,19 +22,42 @@ return inquirer.prompt([{
 
 }
 
+function generateHTML(answers) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <img src='${avatar_url}'>
+        <br>
+        <p>${login}</p>
+        <br>
+        <a href=${location}>Location</a>
+        <a href=${url}>Github link</a>
+        <a href=${blog}>User blog</a>
+        <br>
+        <p>${bio}</p>
+        <br>
+        <p>${public_repos}</p>
+        <br>
+        <p>${followers}</p>
+        <br>
+        <p>number of github stars</p>
+    </body>
+    </html>`;
+  }
+
 promptUser().then(({username}) => {
 
     const queryUrl = `https://api.github.com/users/${username}`;
 
-    axios.get(queryUrl).then(res => {
-        console.log(res);
-    })
-})
+    axios.get(queryUrl).then(answers => {
+        const html = generateHTML(answers);
 
-
-
-    
-
-
-
-
+        return writeFileAsync('index.html', html);
+      })
